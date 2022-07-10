8ddc84a3f9cc9601c3d2a67282d06605f058f8f9
Will Pittman <willjpittman@gmail.com>
2022-07-10 13:55:49 -0400

    Refactors html renderer to exclusively use HTML api

--- internal/renderers/html.go
+++ internal/renderers/html.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/lithammer/dedent"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 	"willpittman.net/x/logger"
@@ -45,23 +44,6 @@ func (html *HTML) Setup(dump *mwdump.XMLDump, outDir string) error {
 
 // Renders one page to HTML, returns as string.
 func (this *HTML) Render(page *mwdump.Page) (rendered string, err error) {
-	// html header
-	header := dedent.Dedent(fmt.Sprintf(`
-		<html>
-		<head>
-		  <title>%s</title>
-		  <link rel="stylesheet" href="%s" />
-		</head>
-		`, page.Title, stylesheetName,
-	))
-
-	// h1
-	title := fmt.Sprintf(
-		"<h1 id=\"%s\">%s</h1>\n",
-		toHtmlId(page.Title),
-		page.Title,
-	)
-
 	// rendered wiki
 	opts := utils.PandocOptions{From: "mediawiki", To: "html"}
 	renderRaw, err := utils.PandocConvert(page, &opts)
@@ -69,47 +51,46 @@ func (this *HTML) Render(page *mwdump.Page) (rendered string, err error) {
 		return "", err
 	}
 
-	// parses/modifies HTML (corrects links, increments header-levels, ...)
+	// parses/modifies/re-renders HTML (correcting links, setting header-levels, ...)
 	node, err := html.Parse(strings.NewReader(renderRaw))
 	if err != nil {
 		utils.LogWarnOn(err)
 		return "", err
 	}
-	finalBody, _ := this.adjustHtmlNode(node)
-
+	node, _ = this.recursiveAdjustHtml(node, page)
 	var render strings.Builder
-	html.Render(&render, finalBody)
+	html.Render(&render, node)
 
-	// end of html
-	footer := `</html>`
-	return fmt.Sprint(header, title, render.String(), footer), nil
-	return "", nil
+	return render.String(), nil
 }
 
-func (this *HTML) adjustHtmlNode(node *html.Node) (finalNode *html.Node, err error) {
+// Rebuilds HTML Tree, adjusted for serving over static html
+func (this *HTML) recursiveAdjustHtml(node *html.Node, page *mwdump.Page) (*html.Node, error) {
+	var err error
+
 	// process
-	newNode, err := this.adjustAnchorLinks(node)
+	node = this.adjustHeadNode(node, page)
+	node = this.adjustBodyNode(node, page)
+	node, err = this.adjustAnchorNode(node)
 	if err != nil {
 		return nil, err
 	}
 
-	// mutate children
+	// recurse through children
 	var children []*html.Node
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
-		newChild, err := this.adjustHtmlNode(child)
+		child, err = this.recursiveAdjustHtml(child, page)
 		if err != nil {
-			return nil, err
+			return child, err
 		}
-		children = append(children, newChild)
+		children = append(children, child)
 	}
 
-	// update this node so points to updated children
+	// point Child/Sibling info in structs to the new children
 	if len(children) > 0 {
-		newNode.FirstChild = children[0]
-		newNode.LastChild = children[len(children)-1]
+		node.FirstChild = children[0]
+		node.LastChild = children[len(children)-1]
 	}
-
-	// now update children so they point to correct sibling
 	for index, child := range children {
 		if 0 < index && index < len(children)-1 {
 			child.PrevSibling = children[index-1]
@@ -117,19 +98,90 @@ func (this *HTML) adjustHtmlNode(node *html.Node) (finalNode *html.Node, err err
 		}
 	}
 
-	return newNode, nil
-}
-
-// Increments the header-level of every HTML header in 'render'.
-// (ex. <h1>foo</h1> --> <h2>foo</h2>)
-func (this *HTML) incrementHeaderLvl(node *html.Node) (finalNode *html.Node, err error) {
 	return node, nil
 }
 
+// Adjusts HTML <head>
+//
+//    - Links stylesheet
+//    - Sets Page Title.
+func (this *HTML) adjustHeadNode(node *html.Node, page *mwdump.Page) *html.Node {
+	if node.Type != html.ElementNode {
+		return node
+	}
+	if node.DataAtom != atom.Head {
+		return node
+	}
+
+	titleVal := html.Node{
+		Type:      html.TextNode,
+		Data:      page.Title,
+		Namespace: node.Namespace,
+	}
+
+	title := html.Node{
+		Type:       html.ElementNode,
+		DataAtom:   atom.Title,
+		Data:       "title",
+		Namespace:  node.Namespace,
+		FirstChild: &titleVal,
+		LastChild:  &titleVal,
+	}
+
+	linkStyle := html.Node{
+		Type:      html.ElementNode,
+		DataAtom:  atom.Link,
+		Data:      "link",
+		Namespace: node.Namespace,
+		Attr: []html.Attribute{
+			{Namespace: node.Namespace, Key: "rel", Val: "stylesheet"},
+			{Namespace: node.Namespace, Key: "href", Val: stylesheetName},
+		},
+	}
+
+	node.AppendChild(&title)
+	node.AppendChild(&linkStyle)
+	return node
+}
+
+// Adjusts HTML Body
+//
+//    - Prepends a <h1> with the page title to the body.
+func (this *HTML) adjustBodyNode(node *html.Node, page *mwdump.Page) *html.Node {
+	if node.Type != html.ElementNode {
+		return node
+	}
+	if node.DataAtom != atom.Body {
+		return node
+	}
+
+	headerVal := html.Node{
+		Type:      html.TextNode,
+		Data:      page.Title,
+		Namespace: node.Namespace,
+	}
+
+	header := html.Node{
+		Type:       html.ElementNode,
+		DataAtom:   atom.H1,
+		Data:       "h1",
+		Namespace:  node.Namespace,
+		FirstChild: &headerVal,
+		LastChild:  &headerVal,
+		Attr: []html.Attribute{
+			{Namespace: node.Namespace, Key: "id", Val: toHtmlId(page.Title)},
+		},
+	}
+
+	node.InsertBefore(&header, node.FirstChild)
+	return node
+}
+
 // Modifes `<a href="">` elements, so they point to files we have written to disk.
-//  - files on disk use POSIX compatible characters in filename
-//  - since serving statically without webserver, appends '.html' to filename
-func (this *HTML) adjustAnchorLinks(node *html.Node) (finalNode *html.Node, err error) {
+//
+//    - files on disk use POSIX compatible characters in filename
+//    - since serving statically without webserver, appends '.html' to filename
+func (this *HTML) adjustAnchorNode(node *html.Node) (finalNode *html.Node, err error) {
 	if node.Type != html.ElementNode {
 		return node, nil
 	}
@@ -173,7 +225,7 @@ func (this *HTML) adjustAnchorLinks(node *html.Node) (finalNode *html.Node, err 
 }
 
 // Increments the header-level of every HTML header in 'render'.
-// (ex. <h1>foo</h1> --> <h2>foo</h2>)
+//   (ex. <h1>foo</h1> --> <h2>foo</h2>)
 func incrHeaders(render string) string {
 	return headerRx.ReplaceAllStringFunc(render, func(match string) string {
 		submatches := headerRx.FindStringSubmatch(match)
@@ -184,7 +236,7 @@ func incrHeaders(render string) string {
 }
 
 // Downcases, and sanitizes characters in a HTML header to assign to html ID.
-// (ex. 'My  Page' --> 'my_page')
+//   (ex. 'My  Page' --> 'my_page')
 func toHtmlId(value string) string {
 	downcased := strings.ToLower(value)
 	return idInvalidRx.ReplaceAllString(downcased, "_")
