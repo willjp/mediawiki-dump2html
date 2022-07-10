d46ccc4ff65f82dc64d671e92bd24a680d29cc04
Will Pittman <willjpittman@gmail.com>
2022-07-10 15:54:22 -0400

    Adds test for SantizeFilename

---
+++ internal/utils/path_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/utils"
+)
+
+func TestSantizePath(t *testing.T) {
+	cases := []struct {
+		test    string
+		path    []byte
+		expects []byte
+	}{
+		{
+			test:    "Valid Paths are not changed",
+			path:    []byte("MyFile-0_1_1.txt"),
+			expects: []byte("MyFile-0_1_1.txt"),
+		},
+		{
+			test:    "Groups of InValid Characters are replaced with underscores",
+			path:    []byte("Subject: Name > Foo!.txt"),
+			expects: []byte("Subject__Name___Foo_.txt"),
+		},
+	}
+	for _, tcase := range cases {
+		t.Run(tcase.test, func(t *testing.T) {
+			res := utils.SanitizeFilename(tcase.path)
+			assert.Equal(t, string(res), string(tcase.expects))
+		})
+	}
+}
