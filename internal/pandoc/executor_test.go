0efd05bb19e00c6c9acef4152ec5bd031ada78ee
Will Pittman <willjpittman@gmail.com>
2022-07-21 18:01:53 -0400

    pandoc/executor_test reuses fake_cmd_executor

--- internal/pandoc/executor_test.go
+++ internal/pandoc/executor_test.go
@@ -6,45 +6,41 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/interfaces"
 	stubs "willpittman.net/x/mediawiki-to-sphinxdoc/internal/test/stubs"
 )
 
-// implements interfaces/PandocExecutor
-type FakePandocExecutor struct {
-	cmd interfaces.CmdExecutor
-}
-
-func (this *FakePandocExecutor) Execute(stdin io.Reader) (render string, errs []error) {
-	result, _ := io.ReadAll(stdin)
-	return string(result), nil
-}
-
-func (this *FakePandocExecutor) Args() []string {
-	return this.cmd.Args()
-}
-
 // tests
 func TestExecute(t *testing.T) {
-	t.Run("Forwards STDIN, and performs cmd.Execute()", func(t *testing.T) {
-		val := "abc"
-		stdin := strings.NewReader(val)
-		cmd := FakePandocExecutor{}
+	t.Run("Forwards STDIN to cmd.Execute()", func(t *testing.T) {
+		stdin := strings.NewReader("abc")
+		cmd := stubs.FakeCmd{}
 		executor := Executor{}
-		result, errs := executor.Execute(&cmd, stdin)
-
+		_, errs := executor.Execute(&cmd, stdin)
 		assert.Nil(t, errs)
-		assert.Equal(t, val, result)
+
+		in, err := io.ReadAll(cmd.Stdin)
+		assert.Nil(t, err)
+		assert.Equal(t, []byte("abc"), in)
+	})
+
+	t.Run("Performs/Wraps cmd.Execute()", func(t *testing.T) {
+		stdin := strings.NewReader("abc")
+		cmd := stubs.FakeCmd{Render: "abc"}
+		executor := Executor{}
+		out, errs := executor.Execute(&cmd, stdin)
+		assert.Nil(t, errs)
+		assert.Equal(t, "abc", out)
 	})
 }
 
 func TestArgs(t *testing.T) {
 	t.Run("Returns contained cmd.Args()", func(t *testing.T) {
+		stdin := strings.NewReader("")
 		args := []string{"foo", "-a", "--verbose"}
 		cmd := stubs.FakeCmd{CliArgs: args}
-		pandocCmd := FakePandocExecutor{cmd: &cmd}
-		executor := Executor{cmd: &pandocCmd}
-
+		executor := Executor{}
+		_, errs := executor.Execute(&cmd, stdin)
+		assert.Nil(t, errs)
 		assert.Equal(t, args, executor.Args())
 	})
 }
--- internal/test/stubs/fake_cmd_executor.go
+++ internal/test/stubs/fake_cmd_executor.go
@@ -1,14 +1,29 @@
 package test
 
-import "io"
+import (
+	"bytes"
+	"io"
+)
 
 // implements interfaces/CmdExecutor
 type FakeCmd struct {
+	Errors  []error
+	Render  string
 	CliArgs []string
+	Stdin   *bytes.Buffer
 }
 
 func (this *FakeCmd) Execute(stdin io.Reader) (render string, errs []error) {
-	return "", nil
+	this.Stdin = bytes.NewBuffer(nil)
+	conts, err := io.ReadAll(stdin)
+	if err != nil {
+		panic(err)
+	}
+	_, err = this.Stdin.Write(conts)
+	if err != nil {
+		panic(err)
+	}
+	return this.Render, this.Errors
 }
 
 func (this *FakeCmd) Args() []string {
