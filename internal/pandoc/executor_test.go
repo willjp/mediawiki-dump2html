4514e1b0e011713d1b3ef7dede25ca788868f78b
Will Pittman <willjpittman@gmail.com>
2022-07-21 17:26:22 -0400

    Adds pandoc executor test coverage

--- internal/pandoc/executor_test.go
+++ internal/pandoc/executor_test.go
@@ -6,21 +6,57 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/interfaces"
 )
 
-type FakeCmdExecutor struct{}
+// implements interfaces/CmdExecutor
+type FakeCmd struct {
+	args []string
+}
+
+func (this *FakeCmd) Execute(stdin io.Reader) (render string, errs []error) {
+	return "", nil
+}
+
+func (this *FakeCmd) Args() []string {
+	return this.args
+}
+
+// implements interfaces/PandocExecutor
+type FakeCmdExecutor struct {
+	cmd interfaces.CmdExecutor
+}
 
 func (this *FakeCmdExecutor) Execute(stdin io.Reader) (render string, errs []error) {
 	result, _ := io.ReadAll(stdin)
 	return string(result), nil
 }
 
-func TestCliExecutor(t *testing.T) {
-	val := "abc"
-	stdin := strings.NewReader(val)
-	cmd := FakeCmdExecutor{}
-	result, errs := cmd.Execute(stdin)
+func (this *FakeCmdExecutor) Args() []string {
+	return this.cmd.Args()
+}
 
-	assert.Nil(t, errs)
-	assert.Equal(t, val, result)
+// tests
+func TestExecute(t *testing.T) {
+	t.Run("Forwards STDIN, and performs cmd.Execute()", func(t *testing.T) {
+		val := "abc"
+		stdin := strings.NewReader(val)
+		cmd := FakeCmdExecutor{}
+		executor := Executor{}
+		result, errs := executor.Execute(&cmd, stdin)
+
+		assert.Nil(t, errs)
+		assert.Equal(t, val, result)
+	})
+}
+
+func TestArgs(t *testing.T) {
+	t.Run("Returns contained cmd.Args()", func(t *testing.T) {
+		args := []string{"foo", "-a", "--verbose"}
+		cmd := FakeCmd{args: args}
+		pandocCmd := FakeCmdExecutor{cmd: &cmd}
+		executor := Executor{cmd: &pandocCmd}
+
+		assert.Equal(t, args, executor.Args())
+	})
 }
