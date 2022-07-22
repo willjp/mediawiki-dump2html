7aeaaf1bf3b5677066480e62ed5c599eee88f2e6
Will Pittman <willjpittman@gmail.com>
2022-07-22 09:28:02 -0400

    Adsd FakeFile, which conforms to interfaces.osFile

--- internal/test/stubs/fake_file.go
+++ internal/test/stubs/fake_file.go
@@ -1,9 +1,26 @@
 package test
 
+import "bytes"
+
 type FakeFile struct {
-	Path string
+	Path       string
+	Buffer     *bytes.Buffer
+	CloseError error
 }
 
-func (this FakeFile) Name() string {
+func NewFakeFile() FakeFile {
+	buf := bytes.NewBuffer(nil)
+	return FakeFile{Buffer: buf}
+}
+
+func (this *FakeFile) Name() string {
 	return this.Path
+}
+
+func (this *FakeFile) WriteString(v string) (n int, err error) {
+	return this.Buffer.WriteString(v)
+}
+
+func (this *FakeFile) Close() error {
+	return this.CloseError
 }
