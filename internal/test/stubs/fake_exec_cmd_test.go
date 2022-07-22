8c79c57b3ba3ef3df059e2644e68a9ef984693ae
Will Pittman <willjpittman@gmail.com>
2022-07-22 16:29:58 -0400

    Extends FakeExecCmd test coverage

--- internal/test/stubs/fake_exec_cmd_test.go
+++ internal/test/stubs/fake_exec_cmd_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,5 +15,40 @@ func TestFakeExecCmd(t *testing.T) {
 		}
 		cmd := FakeExecCmd{}
 		assert.True(t, implementsIface(cmd))
+	})
+
+	t.Run("StdinPipe returns provided Stdin", func(t *testing.T) {
+		writer := strings.Builder{}
+		stdin := FakeWriteCloser{Writer: &writer}
+		cmd := FakeExecCmd{Stdin: &stdin}
+		result, err := cmd.StdinPipe()
+		assert.Nil(t, err)
+		assert.Equal(t, &stdin, result)
+	})
+
+	t.Run("StdoutPipe returns provided Stdout", func(t *testing.T) {
+		stdout := FakeReadCloser{}
+		cmd := FakeExecCmd{Stdout: &stdout}
+		result, err := cmd.StdoutPipe()
+		assert.Nil(t, err)
+		assert.Equal(t, &stdout, result)
+	})
+
+	t.Run("StderrPipe returns provided Stderr", func(t *testing.T) {
+		stderr := FakeReadCloser{}
+		cmd := FakeExecCmd{Stderr: &stderr}
+		result, err := cmd.StderrPipe()
+		assert.Nil(t, err)
+		assert.Equal(t, &stderr, result)
+	})
+
+	t.Run("Start returns nil", func(t *testing.T) {
+		cmd := FakeExecCmd{}
+		assert.Nil(t, cmd.Start())
+	})
+
+	t.Run("Wait returns nil", func(t *testing.T) {
+		cmd := FakeExecCmd{}
+		assert.Nil(t, cmd.Wait())
 	})
 }
