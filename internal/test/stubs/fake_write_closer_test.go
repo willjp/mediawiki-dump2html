044b4670544140e1c6a22a1bb6d6b53c57b97398
Will Pittman <willjpittman@gmail.com>
2022-07-22 16:36:29 -0400

    extends test coverage on FakeWriteCloser

--- internal/test/stubs/fake_write_closer_test.go
+++ internal/test/stubs/fake_write_closer_test.go
@@ -16,6 +16,14 @@ func TestFakeWriterWrite(t *testing.T) {
 		_, err := wCloser.Write([]byte("abc"))
 		assert.Error(t, err, ExpectedError)
 	})
+
+	t.Run("Returns bytes written to Writer on success", func(t *testing.T) {
+		writer := strings.Builder{}
+		wCloser := FakeWriteCloser{Writer: &writer}
+		bytes, err := wCloser.Write([]byte("abc"))
+		assert.Nil(t, err)
+		assert.Equal(t, 3, bytes)
+	})
 }
 
 func TestFakeWriterClose(t *testing.T) {
@@ -24,5 +32,16 @@ func TestFakeWriterClose(t *testing.T) {
 		wCloser := FakeWriteCloser{CloseError: ExpectedError}
 		err := wCloser.Close()
 		assert.Error(t, err, ExpectedError)
+	})
+}
+
+func TestFakeWriterString(t *testing.T) {
+	t.Run("Returns written string", func(t *testing.T) {
+		writer := strings.Builder{}
+		wCloser := FakeWriteCloser{Writer: &writer}
+		_, err := wCloser.Write([]byte("abc"))
+		assert.Nil(t, err)
+		res := wCloser.String()
+		assert.Equal(t, "abc", res)
 	})
 }
