59d1d68933510cfe75d49193c27719ddba23c35c
Will Pittman <willjpittman@gmail.com>
2022-07-22 16:10:44 -0400

    Extends test coverage on FakeCmd

--- internal/test/stubs/fake_cmd_executor_test.go
+++ internal/test/stubs/fake_cmd_executor_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"errors"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,5 +16,23 @@ func TestFakeCmd(t *testing.T) {
 		}
 		cmd := FakeCmd{}
 		assert.True(t, implementsIface(&cmd))
+	})
+
+	t.Run("Returns FakeCmd.Render when provided", func(t *testing.T) {
+		cmd := FakeCmd{Render: "abc"}
+		stdin := strings.NewReader("")
+		res, errs := cmd.Execute(stdin)
+		assert.Nil(t, errs)
+		assert.Equal(t, "abc", res)
+	})
+
+	t.Run("Returns FakeCmd.Errors when provided", func(t *testing.T) {
+		var ExpectedError = errors.New("Expected")
+		cmd := FakeCmd{Errors: []error{ExpectedError}}
+		stdin := strings.NewReader("")
+		res, errs := cmd.Execute(stdin)
+		assert.Equal(t, 1, len(errs))
+		assert.Error(t, ExpectedError, errs[0])
+		assert.Equal(t, "", res)
 	})
 }
