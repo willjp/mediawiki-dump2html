1550f2c1c0fe4040b08670bd1625148a45bc71b5
Will Pittman <willjpittman@gmail.com>
2022-07-29 23:44:15 -0400

    Adds HighlightCSS renderer

---
+++ internal/renderers/html/highlight_css.go
@@ -0,0 +1,89 @@
+package renderers
+
+import (
+	"os"
+	"path"
+	"strings"
+
+	"github.com/spf13/afero"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/appfs"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/interfaces"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/pandoc"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/utils"
+)
+
+// Struct with methods to render pandoc's sytnaxhighlighting CSS stylesheet.
+//
+// Pandoc's style.html templating disables syntaxhighlighting unless it is required.
+// This object generates a minimal amount of stub files for pandoc to render just the
+// CSS for it's syntaxhighlighting.
+//
+// Roughly equivalent to
+//  echo '```html\n<p>placeholder</p>\n```' | pandoc --template=/tmp/dir/highlight-css.template
+type HighlightCSS struct {
+	pandocExecutor interfaces.PandocExecutor
+	tempfileWriter interfaces.FileWriter
+}
+
+func NewHighlightCSS(pandocExecutor interfaces.PandocExecutor) HighlightCSS {
+	return HighlightCSS{
+		pandocExecutor: pandocExecutor,
+		tempfileWriter: &utils.FileWriter{},
+	}
+}
+
+// Renders pandoc's syntaxhighlighting CSS stylesheet
+func (this *HighlightCSS) Render() (render string, errs []error) {
+	var err error
+
+	// dummy markdown file we'll provide to pandoc to create syntaxhighlighting css for
+	htmlSeed := "```html\n<p>placeholder</p>\n```"
+
+	// Dummy pandoc template, that sets '$highlighting-css$'.
+	// Pandoc's 'style.html's preprocessor checks for this to determine if highlighting-css should be generated
+	file, err := this.writeCssTemplate()
+	if err != nil {
+		panic(err)
+	}
+	defer func(file afero.File) {
+		err = file.Close()
+		if err != nil {
+			errs = append(errs, err)
+		}
+		err = appfs.AppFs.Remove(file.Name())
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}(file)
+
+	// Create command to render template
+	opts := pandoc.Opts{Template: file.Name()}
+	cmd := opts.Command()
+	stdin := strings.NewReader(htmlSeed)
+	css, errs := this.pandocExecutor.Execute(&cmd, stdin)
+	if errs != nil {
+		return "", errs
+	}
+
+	// cat htmlSeed.md | pandoc --template={dummy-template}  # output is css
+	return css, nil
+}
+
+// Writes a temporary pandoc template that will enable the generation of syntaxhighlighting-css.
+// Caller is responsible for closing and removing the file.
+func (this *HighlightCSS) writeCssTemplate() (file afero.File, err error) {
+	Os := afero.Afero{Fs: appfs.AppFs}
+	dirpath, err := Os.TempDir(os.TempDir(), "highlight-css-template-")
+	if err != nil {
+		return nil, err
+	}
+
+	// pandoc requires a '.template' suffix
+	filepath := path.Join(dirpath, "highlight-css.template")
+	file, err = Os.Create(filepath)
+	if err != nil {
+		return file, err
+	}
+	_, err = this.tempfileWriter.Write(file, []byte("$highlighting-css$\n"))
+	return file, err
+}
---
+++ internal/renderers/html/highlight_css_test.go
@@ -0,0 +1,105 @@
+package renderers
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"willpittman.net/x/mediawiki-to-sphinxdoc/internal/appfs"
+	test "willpittman.net/x/mediawiki-to-sphinxdoc/internal/test/stubs"
+)
+
+var ExpectedError = errors.New("Expected")
+
+func TestNewHighlightCSS(t *testing.T) {
+	executor := test.FakePandocExecutor{}
+	css := NewHighlightCSS(&executor)
+	assert.NotNil(t, css.pandocExecutor)
+}
+
+func TestHighlightCSSRender(t *testing.T) {
+	setup := func(t *testing.T) {
+		appfs.AppFs = afero.NewMemMapFs()
+		Os := afero.Afero{Fs: appfs.AppFs}
+		err := Os.MkdirAll(os.TempDir(), 0755)
+		assert.Nil(t, err)
+	}
+
+	t.Run("htmlSeed is written to pandoc STDIN", func(t *testing.T) {
+		setup(t)
+		executor := test.FakePandocExecutor{}
+		css := NewHighlightCSS(&executor)
+
+		_, errs := css.Render()
+		assert.Nil(t, errs)
+
+		expects := "```html\n<p>placeholder</p>\n```"
+		out := make([]byte, len(expects))
+		_, err := executor.Stdin.Read(out)
+		assert.Nil(t, err)
+		assert.Equal(t, []byte(expects), out)
+	})
+
+	t.Run("$highlighting-css$ is written to pandoc template tempfile", func(t *testing.T) {
+		setup(t)
+		written := bytes.NewBuffer(nil)
+		executor := test.FakePandocExecutor{}
+		css := HighlightCSS{
+			pandocExecutor: &executor,
+			tempfileWriter: &test.FakeFileWriter{Written: written},
+		}
+
+		_, errs := css.Render()
+		assert.Nil(t, errs)
+
+		out, err := io.ReadAll(written)
+		assert.Nil(t, err)
+		assert.Equal(t, []byte("$highlighting-css$\n"), out)
+	})
+
+	t.Run("pandoc template tempfile is passed as cli argument to pandoc", func(t *testing.T) {
+		setup(t)
+		written := bytes.NewBuffer(nil)
+		executor := test.FakePandocExecutor{}
+		css := HighlightCSS{
+			pandocExecutor: &executor,
+			tempfileWriter: &test.FakeFileWriter{Written: written},
+		}
+
+		_, errs := css.Render()
+		assert.Nil(t, errs)
+
+		args := executor.Args()
+		paramMatch := regexp.MustCompile(`^--template=([a-zA-Z]:|/)`)
+		assert.True(t, paramMatch.Match([]byte(args[1])))
+	})
+
+	t.Run("returns css on render success", func(t *testing.T) {
+		setup(t)
+		returnCss := `
+			code span.vs { color: #4070a0; } /* VerbatimString */
+			code span.wa { color: #60a0b0; font-weight: bold; font-style: italic; } /* Warning */
+			`
+		executor := test.FakePandocExecutor{Render: returnCss}
+		css := NewHighlightCSS(&executor)
+
+		out, errs := css.Render()
+		assert.Nil(t, errs)
+		assert.Equal(t, returnCss, out)
+	})
+
+	t.Run("returns errors on render failure", func(t *testing.T) {
+		setup(t)
+		executor := test.FakePandocExecutor{Errors: []error{ExpectedError}}
+		css := NewHighlightCSS(&executor)
+
+		_, errs := css.Render()
+		assert.Equal(t, 1, len(errs))
+		assert.Error(t, ExpectedError, errs[0])
+	})
+}
